638f8c2c2960528c9e0654c9edecd9994d5a14a4
Alanine <kshuo4747@gmail.com>
2026-04-07 19:55:34 +0800

    ip

--- control_terminal/src/config/index.js
+++ control_terminal/src/config/index.js
@@ -5,7 +5,7 @@
 
 // API服务器基础URL
 // 开发环境使用localhost，生产环境请修改为实际服务器地址
-export const BASE_URL = 'http://116.205.106.95:8080'
+export const BASE_URL = 'http://localhost:8080'
 
 // 请求超时时间（毫秒）
 export const TIMEOUT = 10000
